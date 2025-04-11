bb461ce85a89b23f808cd2cd148a44bb00903299
Mariam Sikharulidze <mariam.sikha.22@gmail.com>
2025-04-11 12:33:53 +0400

    Add styles and update card info

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-
-    <Card
-      name="Eminem"
-      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRqUv3bFkqjj9bitaId1Mq5JCT9fVCExtMZA&s"
-      tel="+123 456 789"
-      email="eminem@shadyrecords.com"
-    />
-
-    <Card
-      name="Jack Bauer"
-      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      tel="+987 654 321"
-      email="jack@nowhere.com"
-    />
-
-    <Card
-      name="Chuck Norris"
-      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      tel="+918 372 574"
-      email="gmail@chucknorris.com"
-    />
-  </div>,
-  document.getElementById("root")
-);
